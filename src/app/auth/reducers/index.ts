c17a7f7b5d0fca4ee6171b6645a391092c285571
Dheeraj Kumar <kumardh@transitioncomputing.com>
2020-09-10 21:27:16 +0530

    Initial Commit

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,17 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+
+export interface AppState {
+
+}
+
+export const reducers: ActionReducerMap<AppState> = {
+
+};
+
+
